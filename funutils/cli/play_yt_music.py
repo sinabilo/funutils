a03493e6aa02d40f947fd791acd8ec5f95039fdd
sinabilo <siblo@naver.com>
2018-05-30 18:31:43 +0900

    a

--- funutils/cli/play_yt_music.py
+++ funutils/cli/play_yt_music.py
@@ -1,2 +1,27 @@
 from __future__ import unicode_literals
 import youtube_dl
+
+
+
+
+url = "https://www.youtube.com/watch?v=FJfFZqTlWrQ"
+
+    
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+    # 'format': 'bestaudio/best',       
+    # 'outtmpl': '%(id)s',        
+    'noplaylist' : True,        
+    # 'progress_hooks': [my_hook],  
+    'outtmpl' : "pk01-"
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    a = ydl.download([url])
+    
+import pathlib
+rr = os.path.realpath("pk01-.mkv")
+os.system("mplayer -novideo {}".format(rr))
